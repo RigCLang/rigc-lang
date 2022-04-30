f294b3a4f284bedca45f2a7174ab6741bb95901d
B4mbus <peaceofloneliness@gmail.com>
2022-04-30 14:09:44 +0200

    Improved keyword definition mechanism a bit.

--- Parser/include/RigCParser/Grammar/Keywords.hpp
+++ Parser/include/RigCParser/Grammar/Keywords.hpp
@@ -2,32 +2,32 @@
 
 #include RIGCPARSER_PCH
 
-#define DEFINE_KEYWORD(name, ...) \
-	struct name##Keyword \
-		: pegtl::keyword< __VA_ARGS__ > \
+#define DEFINE_KEYWORD(structName, keywordName) \
+	struct structName##Keyword \
+		: TAO_PEGTL_KEYWORD( keywordName ) \
 	{ }
 
 namespace rigc
 {
 
-DEFINE_KEYWORD(From,		'f','r','o','m');
-DEFINE_KEYWORD(Import,		'i','m','p','o', 'r', 't');
-DEFINE_KEYWORD(Export,		'e','x','p','o','r', 't');
-DEFINE_KEYWORD(Func,		'f','u','n','c');
-DEFINE_KEYWORD(Var,			'v','a','r');
-DEFINE_KEYWORD(Const,		'c','o','n','s','t');
-DEFINE_KEYWORD(If,			'i','f');
-DEFINE_KEYWORD(Else,		'e','l','s','e');
-DEFINE_KEYWORD(While,		'w','h','i','l','e');
-DEFINE_KEYWORD(Do,			'd','o');
-DEFINE_KEYWORD(For,			'f','o','r');
-DEFINE_KEYWORD(Ret,			'r','e','t');
-DEFINE_KEYWORD(True,		't','r','u','e');
-DEFINE_KEYWORD(False,		'f','a','l','s','e');
-DEFINE_KEYWORD(Class,		'c','l','a','s','s');
-DEFINE_KEYWORD(Override,	'o','v','e','r','r','i','d','e');
-DEFINE_KEYWORD(Template,	't','e','m','p','l','a','t','e');
-DEFINE_KEYWORD(TemplateTypename,	't','y','p','e','_','n','a','m','e');
+DEFINE_KEYWORD(From,		"from");
+DEFINE_KEYWORD(Import,		"import");
+DEFINE_KEYWORD(Export,		"export");
+DEFINE_KEYWORD(Func,		"func");
+DEFINE_KEYWORD(Var,			"var");
+DEFINE_KEYWORD(Const,		"const");
+DEFINE_KEYWORD(If,			"if");
+DEFINE_KEYWORD(Else,		"else");
+DEFINE_KEYWORD(While,		"while");
+DEFINE_KEYWORD(Do,			"do");
+DEFINE_KEYWORD(For,			"for");
+DEFINE_KEYWORD(Ret,			"ret");
+DEFINE_KEYWORD(True,		"true");
+DEFINE_KEYWORD(False,		"false");
+DEFINE_KEYWORD(Class,		"class");
+DEFINE_KEYWORD(Override,	"override");
+DEFINE_KEYWORD(Template,	"template");
+DEFINE_KEYWORD(TemplateTypename,	"type_name");
 
 }
 
