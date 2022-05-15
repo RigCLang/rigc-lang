577c6f9f18b525222af451a74291f962a7c11e7b
B4mbus <peaceofloneliness@gmail.com>
2022-05-15 16:44:21 +0200

    fix(vm): replace memcpy with placement new in allocateStaticValue.

--- VM/include/RigCVM/Alloc.hpp
+++ VM/include/RigCVM/Alloc.hpp
@@ -11,7 +11,7 @@ auto allocateStaticValue(DeclType type_, T const& value_) -> Value {
 	auto const size = type_->size();
 
 	auto const data = new std::byte[size];
-	std::memcpy(data, reinterpret_cast<void const*>(value_), size);
+	new (data) T(value_);
 
 	return Value { type_, data };
 }
