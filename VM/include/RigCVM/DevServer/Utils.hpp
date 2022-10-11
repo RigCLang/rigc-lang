729c16554c64a54a09bfaa8c2e4b985a247a85c4
PoetaKodu <rfpawelsyska@gmail.com>
2022-10-11 16:58:02 +0200

    feat(vm): `devserverLog` function

--- VM/include/RigCVM/DevServer/Utils.hpp
+++ VM/include/RigCVM/DevServer/Utils.hpp
@@ -2,9 +2,19 @@
 
 #include RIGCVM_PCH
 
+#include <fmt/color.h>
+
 namespace rigc::vm
 {
 
 auto formatStackFrameLabel(ParserNode const&) -> std::string;
+
+template <typename T, typename... Ts>
+inline void devserverLog(T&& t, Ts&&... ts)
+{
+	using fmt::emphasis, fmt::color;
+	fmt::print(emphasis::bold | fmt::fg(color::aquamarine), "[DevServer] ");
+	fmt::print(fmt::fg(color::aquamarine), std::forward<T>(t), std::forward<Ts>(ts)...);
+}
 
 }
