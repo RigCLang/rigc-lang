dc680683076a7e6f5dac8b41a11498db5890b7f4
PoetaKodu <rfpawelsyska@gmail.com>
2022-10-12 23:10:07 +0200

    refactor(vm): usage of custom aliases doc(vm): more comments

--- VM/include/RigCVM/Stack.hpp
+++ VM/include/RigCVM/Stack.hpp
@@ -8,17 +8,21 @@
 namespace rigc::vm
 {
 
+/// @brief Represents a stack of a thread within a VM.
 struct Stack
 {
 	using DataContainer		= DynArray<char>;
 	using FrameContainer	= DynArray<StackFrame>;
 
-	DataContainer	container;
-	FrameContainer	frames;
+	/// @brief The data of the stack.
+	DataContainer container;
 
-	// Do not use stack.size(), resize etc to
-	// be 100% sure that it won't reallocate
-	size_t		size = 0;
+	/// @brief The frames of the stack.
+	FrameContainer frames;
+
+	/// Do not use stack.size(), resize etc to
+	/// be 100% sure that it won't reallocate
+	size_t size = 0;
 
 	auto data() -> char*
 	{
@@ -30,6 +34,7 @@ struct Stack
 		return container.data();
 	}
 
+	/// @brief Creates a new frame starting at the current stack size.
 	auto pushFrame() -> StackFrame&
 	{
 		auto frame = StackFrame(this);
@@ -39,6 +44,7 @@ struct Stack
 		return frames.back();
 	}
 
+	/// @brief Removes the topmost frame.
 	auto popFrame() -> void
 	{
 		size = frames.back().initialStackSize;
