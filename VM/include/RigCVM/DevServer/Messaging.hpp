9488866892917e18469957277d8bb93793f44ecb
PoetaKodu <rfpawelsyska@gmail.com>
2022-10-16 14:10:10 +0200

    fix(vm): missing `fmt::runtime` when formatting a runtime string

--- VM/include/RigCVM/DevServer/Messaging.hpp
+++ VM/include/RigCVM/DevServer/Messaging.hpp
@@ -21,7 +21,7 @@ void sendLogMessage(LogLevel level_, StringView msg_);
 template <typename Arg, typename... Args>
 void sendLogMessage(LogLevel level_, StringView msg_, Arg&& arg_, Args&&... args_)
 {
-	sendLogMessage(level_, fmt::format(msg_, std::forward<Arg>(arg_), std::forward<Args>(args_)...));
+	sendLogMessage(level_, fmt::format(fmt::runtime(msg_), std::forward<Arg>(arg_), std::forward<Args>(args_)...));
 }
 
 }
