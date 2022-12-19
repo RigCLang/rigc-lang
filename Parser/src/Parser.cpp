5460541ec6d0bd88b5c3f1bbcc6c67a2e62284b6
PoetaKodu <poetakodupl@gmail.com>
2022-12-19 21:13:23 +0100

    fix: do not handle parsing exceptions directly (for now)

--- Parser/src/Parser.cpp
+++ Parser/src/Parser.cpp
@@ -9,27 +9,26 @@ namespace rigc
 auto parse(p::file_input<> &in) -> ParserNodePtr
 {
 	namespace pt = pegtl::parse_tree;
+	return pt::parse< rigc::Grammar, rigc::Selector >( in );
 
-	try
-	{
-		// The actual parser, tracer, parse tree, ...
-		return pt::parse< rigc::Grammar, rigc::Selector >( in );
-	}
-	catch (const pegtl::parse_error &e)
-	{
-		// This catch block needs access to the input
-		auto const p = e.positions().front();
-		std::cerr << e.what() << '\n'
-					<< in.line_at(p) << '\n'
-					<< std::setw(p.column) << '^' << std::endl;
-	}
-	catch (const std::exception &e)
-	{
-		// Generic catch block for other exceptions
-		std::cerr << e.what() << std::endl;
-	}
+	// For now leave the error handling to the caller.
 
-	return nullptr;
+	// try
+	// {
+	// }
+	// catch (const pegtl::parse_error &e)
+	// {
+	// 	auto const p = e.positions().front();
+	// 	std::cerr << e.what() << '\n'
+	// 				<< in.line_at(p) << '\n'
+	// 				<< std::setw(p.column) << '^' << std::endl;
+	// }
+	// catch (const std::exception &e)
+	// {
+	// 	std::cerr << e.what() << std::endl;
+	// }
+
+	// return nullptr;
 }
 
 }
